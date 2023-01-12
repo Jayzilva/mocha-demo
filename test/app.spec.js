d6421178b8cf1a088a103d25824d0dc99c2f50ab
FIRST_NAME LAST_NAME <gitdevjay@gmail.com>
2023-01-12 18:57:30 +0530

    update

---
+++ test/app.spec.js
@@ -0,0 +1,19 @@
+const {add, sub} = require("../src/app");
+const expect = require("chai").expect;
+
+//BDD
+describe("Suits 1", ()=>{
+    it('add(2,3) should return 5', ()=>{
+        expect(add(2,3)).to.be.equal(5);
+    })
+})
+
+
+//TDD
+const {suite, test} = require("mocha");
+
+suite("Suits 2", ()=>{
+    test('add(3,3) should return 6', ()=>{
+        expect(add(3,3)).to.be.equal(6);
+    })
+})
